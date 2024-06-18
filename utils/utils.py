73f6279be4edea879bb2a4902eec19fa7e7266fc
Talia-James <dr.talia.james@gmail.com>
2024-06-18 14:53:23 -0400

    skill max/min implemented and success track

--- utils/utils.py
+++ utils/utils.py
@@ -37,7 +37,55 @@ min_max_dict = {
     'Luck': {'min': 0, 'max': 100, 'derived': False}, 
     'Hit Points': {'min': 0, 'max': 100, 'derived': True}, 
     'Magic Points': {'min': 0, 'max': 100, 'derived': True}, 
-    'Sanity': {'min': 0, 'max': 100, 'derived': True}
+    'Sanity': {'min': 0, 'max': 100, 'derived': True},
+    'Accounting': {'min': 5, 'max': 100, 'derived': False},
+    'Animal Handling': {'min': 1, 'max': 100, 'derived': False},
+    'Anthropology': {'min': 1, 'max': 100, 'derived': False},
+    'Appraise': {'min': 5, 'max': 100, 'derived': False},
+    'Archaeology': {'min': 1, 'max': 100, 'derived': False},
+    'Charm': {'min': 15, 'max': 100, 'derived': False},
+    'Climb': {'min': 20, 'max': 100, 'derived': False},
+    'Computer Use': {'min': 5, 'max': 100, 'derived': False},
+    'Credit Rating': {'min': 0, 'max': 100, 'derived': False},
+    'Cthulhu Mythos': {'min': 0, 'max': 100, 'derived': False},
+    'Disguise': {'min': 5, 'max': 100, 'derived': False},
+    'Dodge': {'min': 0, 'max': 100, 'derived': True},
+    'Drive Auto': {'min': 20, 'max': 100, 'derived': False},
+    'Electrical Repair': {'min': 1, 'max': 100, 'derived': False},
+    'Electronics': {'min': 1, 'max': 100, 'derived': False},
+    'Fast Talk': {'min': 5, 'max': 100, 'derived': False},
+    'Brawl': {'min': 25, 'max': 100, 'derived': False},
+    'Firearms (Small Arms)': {'min': 20, 'max': 100, 'derived': False},
+    'Firearms (Rifle/Shotgun)': {'min': 25, 'max': 100, 'derived': False},
+    'First Aid': {'min': 30, 'max': 100, 'derived': False},
+    'History': {'min': 5, 'max': 100, 'derived': False},
+    'Intimidate': {'min': 15, 'max': 100, 'derived': False},
+    'Jump': {'min': 20, 'max': 100, 'derived': False},
+    'English': {'min': 20, 'max': 100, 'derived': False},
+    'French': {'min': 1, 'max': 100, 'derived': False},
+    'Law': {'min': 5, 'max': 100, 'derived': False},
+    'Library Use': {'min': 20, 'max': 100, 'derived': False},
+    'Listen': {'min': 20, 'max': 100, 'derived': False},
+    'Locksmith': {'min': 1, 'max': 100, 'derived': False},
+    'Mechanical Repair': {'min': 10, 'max': 100, 'derived': False},
+    'Medicine': {'min': 1, 'max': 100, 'derived': False},
+    'Natural World': {'min': 10, 'max': 100, 'derived': False},
+    'Navigate': {'min': 10, 'max': 100, 'derived': False},
+    'Occult': {'min': 5, 'max': 100, 'derived': False},
+    'Operate Heavy Machinery': {'min': 1, 'max': 100, 'derived': False},
+    'Persuade': {'min': 10, 'max': 100, 'derived': False},
+    'Pilot': {'min': 1, 'max': 100, 'derived': False},
+    'Psychoanalysis': {'min': 1, 'max': 100, 'derived': False},
+    'Psychology': {'min': 10, 'max': 100, 'derived': False},
+    'Read Lips': {'min': 1, 'max': 100, 'derived': False},
+    'Ride': {'min': 5, 'max': 100, 'derived': False},
+    'Sleight of Hand': {'min': 10, 'max': 100, 'derived': False},
+    'Spot Hidden': {'min': 25, 'max': 100, 'derived': False},
+    'Stealth': {'min': 20, 'max': 100, 'derived': False},
+    'Survival': {'min': 1, 'max': 100, 'derived': False},
+    'Swim': {'min': 1, 'max': 100, 'derived': False},
+    'Throw': {'min': 20, 'max': 100, 'derived': False},
+    'Track': {'min': 10, 'max': 100, 'derived': False}
 }
 
 def derive(df,func,limit):
@@ -56,4 +104,9 @@ def derive(df,func,limit):
             val = int(99 - df['Cthulhu Mythos'])
         elif limit == 'min':
             val = 0
+    elif func == 'Dodge':
+        if limit == 'min':
+            val = round(df['Dexterity']/2)
+        elif limit == 'max':
+            val = 100
     return val
